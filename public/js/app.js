326784e8a5e69699778c848d044cf0dee1f3ff27
Lyra Von Rose <lyravonrose@gmail.com>
2022-01-03 16:36:36 +0100

    need to update css

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-import * as Vue from './vue.js';
+import * as Vue from "./vue.js";
+
+Vue.createApp({
+    data() {
+        return {
+            heading: "Friends",
+            show: true,
+            images: [],
+        };
+    },
+    mounted() {
+        console.log("app.js just mounted");
+        fetch("/get-images")
+            .then((resp) => resp.json())
+            .then((data) => {
+                this.images = data;
+            });
+    },
+}).mount("#main");
